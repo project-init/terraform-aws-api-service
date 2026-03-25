935adaeef59ff60c0dace492c35e632f6d18009c
Khalil K <wip@khalilonline.com>
2026-03-25 11:07:29 -0500

    feat: Supporting deploying sidecars in ecs task definition (#5)
    * feat: Supporting deploying sidecars in ecs task definition
    
    * fix examples module ref

--- examples/sidecars/main.tf
+++ examples/sidecars/main.tf
@@ -29,6 +29,33 @@ module "grpc_service" {
   cpu    = 256
   memory = 256
 
+  sidecars = [
+    {
+      name      = "shuttle"
+      image     = "111111111111.dkr.ecr.us-east-1.amazonaws.com/shuttle:v0.1.0"
+      essential = false
+      cpu       = 128
+      memory    = 256
+      port_mappings = [
+        { containerPort = 4317, hostPort = 4317, protocol = "tcp" }
+      ]
+      environment_variables = [
+        {
+          name  = "VERSION"
+          value = "us-east-1"
+        },
+      ]
+      secrets = [
+        {
+          name      = "TEST_SECRET",
+          valueFrom = "${module.grafana_secret.secret_arn}:TEST_SECRET::"
+        },
+      ]
+    },
+  ]
+  sidecars_cpu    = 256
+  sidecars_memory = 256
+
   desired_count = 1
   min_capacity  = 1
   max_capacity  = 1
